844f5ae8d87972c66d07ddb5d3897cd4d704c950
AnnaA <andreychenko.a@gmail.com>
2024-12-14 16:47:03 +0300

    Added top10 billboard

---
+++ billboard.R
@@ -0,0 +1,10 @@
+library(readr)
+library(dplyr)
+
+top100 <- read_csv("data/billboard100.csv")
+top10 <- top100 |>
+  group_by(Date) |>
+  filter(row_number() <= 10) |>
+  ungroup()
+
+write_csv(top10,"billboard.csv")
