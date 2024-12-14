c8778b2e3144324bdd177db8e9c97e2b7e325a3c
AnnaA <andreychenko.a@gmail.com>
2024-12-14 17:01:24 +0300

    deleted data folder

--- billboard.R
+++ billboard.R
@@ -1,7 +1,7 @@
 library(readr)
 library(dplyr)
 
-top100 <- read_csv("data/billboard100.csv")
+top100 <- read_csv("billboard100.csv")
 top10 <- top100 |>
   group_by(Date) |>
   filter(row_number() <= 10) |>
